ff51fa64e7f6d97cce4897599ff3f6a49e536db1
Gabriel Leite <gabrielleite82@hotmail.com>
2024-09-02 22:42:31 -0300

    download tamplate

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Main;
+use App\Http\Controllers\TemplatesController;
 
-Route::get('/', [Main::class, 'index'])->name('index');
+// Route::get('/', [Main::class, 'index'])->name('index');
+Route::get('/', [TemplatesController::class, 'index']);
